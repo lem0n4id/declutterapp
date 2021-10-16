aa80059b02b9aad287a89dcfdcbe7e69fe7c2140
lem0n4id <61219881+lem0n4id@users.noreply.github.com>
2021-10-17 02:12:51 +0530

    sawo api

--- declutterapp/views.py
+++ declutterapp/views.py
@@ -31,3 +31,46 @@ def entry():
 @app.route('/settings')
 def settings():
     return render_template("settings.html")
+
+
+createTemplate("templates/partials", flask=True)
+load = ''
+loaded = 0
+
+def setPayload(payload):
+    global load
+    load = payload
+
+
+def setLoaded(reset=False):
+    global loaded
+    if reset:
+        loaded = 0
+    else:
+        loaded += 1
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/login_page")
+def login_page():
+    setLoaded()
+    setPayload(load if loaded < 2 else '')
+    sawo = {
+        "auth_key": API_KEY,
+        "to": "login",
+        "identifier": "email"
+    }
+    return render_template("login.html", sawo=sawo, load=load)
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    payload = json.loads(request.data)["payload"]
+    setLoaded(True)
+    setPayload(payload)
+    status = 200 if(verifyToken(payload)) else 404
+    return {"status": status}
+

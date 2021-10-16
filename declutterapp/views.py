a438a1c47c9daf9bd430f7b7a47f428d8c7b3f92
lem0n4id <61219881+lem0n4id@users.noreply.github.com>
2021-10-16 18:22:10 +0530

    added html files and updated view

--- declutterapp/views.py
+++ declutterapp/views.py
@@ -4,3 +4,23 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/greet')
+def hello():
+    return render_template("greetings.html")
+
+@app.route('/details')
+def fill_details():
+    return render_template("details.html")
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+@app.route('/entry')
+def entry():
+    return render_template("add_entry.html")
+
+@app.route('/settings')
+def settings():
+    return render_template("settings.html")

c56669e9b9bbe1088d44cd44e5eb0ddeec527037
lem0n4id <61219881+lem0n4id@users.noreply.github.com>
2021-10-17 01:04:48 +0530

    remove package

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn declutterapp:app
+web: gunicorn app:app
--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from declutterapp import app
-from flask import config, render_template
+from flask import render_template
+
+from flask import Flask
+app = Flask(__name__)
     
 from sawo import createTemplate, verifyToken
 import json

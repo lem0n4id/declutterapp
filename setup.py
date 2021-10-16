c94bd7e1fb2c35d1069560c80f5cd536e7899e6f
lem0n4id <61219881+lem0n4id@users.noreply.github.com>
2021-10-16 17:34:37 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='declutterapp',
+    packages=['declutterapp'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
